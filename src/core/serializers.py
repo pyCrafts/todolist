86d0cb38fda2db50e95cb76b2316c0609872231e
Oleg <oleg.brest.1997@gmail.com>
2024-08-06 10:21:09 +0000

    AOuth2.0

--- src/core/serializers.py
+++ src/core/serializers.py
@@ -1,29 +1,43 @@
-from django.contrib.auth.password_validation import validate_password
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
-from core.models import User
 from rest_framework.exceptions import ValidationError
+
+from core.models import User
+
 
 class PasswordField(serializers.CharField):
     def __init__(self, **kwargs):
-        kwargs['style'] = {"input_type": 'password'}
-        kwargs.setdefault('write_only', True)
+        kwargs["style"] = {"input_type": "password"}
+        kwargs.setdefault("write_only", True)
         super().__init__(**kwargs)
         self.validators.append(validate_password)
+
 
 class CreateUserSerializer(serializers.ModelSerializer):
     password = PasswordField(required=True)
     password_repeat = PasswordField(required=True)
+
     class Meta:
         model = User
-        fields = ("id", "username", "first_name", "last_name", "email", "password", "password_repeat")
+        fields = (
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "password",
+            "password_repeat",
+        )
 
     def validate(self, attrs: dict):
         if attrs["password"] != attrs["password_repeat"]:
             raise ValidationError("Password must match")
         return attrs
-    
+
     def create(self, validated_data: dict):
-        del validated_data["password_repeat"]# удаление
-        validated_data["password"] = make_password(validated_data["password"]) # шифровка пароля
+        del validated_data["password_repeat"]  # удаление
+        validated_data["password"] = make_password(
+            validated_data["password"]
+        )  # шифровка пароля
         return super().create(validated_data)
--- src/core/urls.py
+++ src/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+
 from core import views
 
-urlpatterns = [ 
-       path("signup", views.SignupView.as_view(), name="signup"),
+urlpatterns = [
+    path("signup", views.SignupView.as_view(), name="signup"),
 ]
