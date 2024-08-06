7484132e7dc46e706a6a02a8800108dc500b4bf3
Oleg <oleg.brest.1997@gmail.com>
2024-08-06 20:54:11 +0000

    AOuth2.0

--- src/core/serializers.py
+++ src/core/serializers.py
@@ -1,7 +1,8 @@
+from django.contrib.auth import authenticate
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import AuthenticationFailed, ValidationError, NotAuthenticated
 
 from core.models import User
 
@@ -41,3 +42,46 @@ class CreateUserSerializer(serializers.ModelSerializer):
             validated_data["password"]
         )  # шифровка пароля
         return super().create(validated_data)
+    
+
+class LoginSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=True)
+    password = PasswordField(required=True)
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+    def create(self, validated_data: dict):
+        if not (user := authenticate(
+                username=validated_data['username'],
+                password=validated_data['password']
+        )):
+            raise AuthenticationFailed
+        return user
+    
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "username", "first_name", "last_name", "email")
+
+
+class UpdatePasswordSerializer(serializers.Serializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    old_password = PasswordField(required=True)
+    new_password = PasswordField(required=True)
+
+    def validate(self, attrs: dict):
+        if not (user := attrs["user"]):
+            raise NotAuthenticated
+        if not user.check_password(attrs["old_password"]):
+            raise ValidationError({"old password": "field is incorrect"})
+        return attrs
+    
+    def create(self, validated_data: dict) -> User:
+        raise NotImplementedError
+    
+    def update(self, instance: User, validated_data: dict) -> User:
+        instance.password = make_password(validated_data["new_password"])
+        instance.save(update_fields=("password", ))
+        return instance
--- src/core/views.py
+++ src/core/views.py
@@ -1,9 +1,39 @@
+from django.contrib.auth import login, logout
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
+import rest_framework.permissions as permissions
 
-from core.serializers import CreateUserSerializer
+from core.serializers import CreateUserSerializer, LoginSerializer, ProfileSerializer, UpdatePasswordSerializer
+from core.models import User
 
 
 # Create your views here.
 class SignupView(generics.CreateAPIView):
     serializer_class = CreateUserSerializer
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        login(request=request, user=serializer.save())
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+class ProfileView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ProfileSerializer
+    queryset = User.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+    def get_object(self) -> User:
+        return self.request.user
+    def delete(self, request, *args, **kwargs):
+        logout(request)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+class UpdatePasswordView(generics.UpdateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UpdatePasswordSerializer
+
+    def get_object(self): 
+        return self.request.user
